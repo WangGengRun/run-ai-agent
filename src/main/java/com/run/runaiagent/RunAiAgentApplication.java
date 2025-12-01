46bf9366d85495da02c9d0e0034dc4a1afe70d51
王庚润 <3206201719@qq.com>
2025-12-01 10:57:03 +0800

    首次提交：初始化项目

---
+++ src/main/java/com/run/runaiagent/RunAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.run.runaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/run/runaiagent/RunAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.run.runaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
