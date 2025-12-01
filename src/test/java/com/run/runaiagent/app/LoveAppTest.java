2103efa32526eb96fed1ea6e59fd9affb91d5c27
王庚润 <3206201719@qq.com>
2025-12-01 20:01:43 +0800

    开发自定义日志拦截器

---
+++ src/test/java/com/run/runaiagent/app/LoveAppTest.java
@@ -0,0 +1,34 @@
+package com.run.runaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+
+        String message = "你好，我是程序员RUN";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+
+        message = "我想让另一半(Money)更爱我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
