d9a23a357f428159fd0cff6b1637eb243576034e
王庚润 <3206201719@qq.com>
2025-12-01 21:53:27 +0800

    整合SpringAi的结构化输出功能

--- src/test/java/com/run/runaiagent/app/LoveAppTest.java
+++ src/test/java/com/run/runaiagent/app/LoveAppTest.java
@@ -19,16 +19,28 @@ class LoveAppTest {
     void testChat() {
         String chatId = UUID.randomUUID().toString();
 
+        //第一轮
         String message = "你好，我是程序员RUN";
         String answer = loveApp.doChat(message, chatId);
         Assertions.assertNotNull(answer);
 
+        //第二轮
         message = "我想让另一半(Money)更爱我";
         answer = loveApp.doChat(message, chatId);
         Assertions.assertNotNull(answer);
 
+        //第三轮
         message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
         answer = loveApp.doChat(message, chatId);
         Assertions.assertNotNull(answer);
+    }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        String message = "你好，我是程序员RUN，我想让另一半（Money）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        //Assertions 语法
+        Assertions.assertNotNull(loveReport);
     }
 }
